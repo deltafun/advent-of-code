c284697ec019a448499d66d71466b30cdbefb7cb
deltafun <deltafun@users.noreply.github.com>
2018-12-03 23:55:37 -0500

    Day1: Remove disgusting tabs

--- 01/01-chronal-calibration.cpp
+++ 01/01-chronal-calibration.cpp
@@ -8,43 +8,43 @@
 using namespace std;
 
 vector<int> GetFrequencies() {
-	ifstream infile("../input/01.txt");
-	string buf;
-	vector<int> freqs;
+  ifstream infile("../input/01.txt");
+  string buf;
+  vector<int> freqs;
 
-	if (!infile) {
-		cout << "Can't read input";
-		exit(1);
-	}
+  if (!infile) {
+    cout << "Can't read input";
+    exit(1);
+  }
 
-	while (getline(infile, buf)) {
-		freqs.emplace_back(stoi(buf));
-	}
+  while (getline(infile, buf)) {
+    freqs.emplace_back(stoi(buf));
+  }
 
-	return freqs;
+  return freqs;
 }
 
 void AccioAberration(const vector<int>& freqs) {
-	unordered_set<int> seen;
-	int sum = 0;
-	seen.emplace(0);
+  unordered_set<int> seen;
+  int sum = 0;
+  seen.emplace(0);
 
-	while (true) {
-		for (const int& x : freqs) {
-			sum += x;
-			auto isEmplaced = seen.emplace(sum);
-			
-			if (!isEmplaced.second) {
-				cout << sum << endl;
-				return;
-			}
-		}
-	}
+  while (true) {
+    for (const int& x : freqs) {
+      sum += x;
+      auto isEmplaced = seen.emplace(sum);
+
+      if (!isEmplaced.second) {
+        cout << sum << endl;
+        return;
+      }
+    }
+  }
 }
 
 int main() {
-	AccioAberration(GetFrequencies());
+  AccioAberration(GetFrequencies());
 
-	getchar();
-	return 0;
-}                                                                              
+  getchar();
+  return 0;
+}
