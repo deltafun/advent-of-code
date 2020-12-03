5324c421dc20a3e38756491d247ad2542695558a
deltafun <deltafun@users.noreply.github.com>
2020-12-03 00:08:31 -0500

    2020/01: part two

--- 2020/01/report-repair.py
+++ 2020/01/report-repair.py
@@ -17,8 +17,25 @@ def show_me_the_money(big_money):
     return None
 
 
+def show_me_the_money_part_two(big_money):
+    for expense in big_money:
+        target = magic_number - expense
+        for x in big_money:
+            is_this_it = target - x
+            if is_this_it == x or is_this_it == expense:
+                # TODO: Is this actually impossible?
+                print(f'is_this_it={is_this_it} which equals expense={expense} or x={x}')
+                continue
+            if is_this_it in big_money:
+                return expense, x, is_this_it
+    print('huh?')
+    return None
+
+
 if __name__ == '__main__':
     lol = get_input(input1)
     ans = show_me_the_money(lol)
     print(ans[0] * ans[1])
+    ans2 = show_me_the_money_part_two(lol)
+    print(ans2[0] * ans2[1] * ans2[2])
 
